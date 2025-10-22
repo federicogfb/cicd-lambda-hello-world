697fee010db641f77bf5a5a5970f1a2fcd3fdaa7
federicogfb <fedelibra98@gmail.com>
2025-10-22 15:16:12 -0300

    push: 2nd push

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const { v4: uuidv4 } = require('uuid');
+
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
             message: 'Hello World from AWS Lambda!',
+            requestId: uuidv4(),
             timestamp: new Date().toISOString()
         }),
     };
